d162696399172bf00d7079e02dd5f240346026b3
André Ricardo <contatoadmx@gmail.com>
2023-10-25 03:50:48 -0300

    mx install front

--- source/command/install/front.php
+++ source/command/install/front.php
@@ -7,13 +7,15 @@ namespace Elegance;
 return function () {
     Terminal::run('composer');
 
-    Terminal::run('install.structure');
+    Terminal::run('create.structure');
 
     Terminal::run('install.index');
 
-    File::copy('#elegance-server/assets/favicon.ico', 'assets/favicon.ico');
+    File::copy('#elegance-server/public/favicon.ico', 'public/favicon.ico');
 
-    Dir::copy('#elegance-front/assets', 'assets');
+    File::copy('#elegance-front/source/routers/front.php', 'source/routers/front.php');
+
+    Dir::copy('#elegance-front/public', 'public');
 
     Dir::copy('#elegance-front/view/front', 'view/front');
 
---
+++ source/routes/front.php
@@ -0,0 +1,9 @@
+<?php
+
+use Elegance\Router;
+
+Router::middleware(['elegance.front'], function () {
+
+    //Your font routers here
+
+});
