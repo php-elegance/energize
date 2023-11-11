f0366e7c5a62e3b8dd8f534ecfaae9e277e80501
André Ricardo <contatoadmx@gmail.com>
2023-11-11 16:20:48 -0300

    fx response redirect energize

--- source/Middleware/MidEnergize.php
+++ source/Middleware/MidEnergize.php
@@ -16,16 +16,40 @@ class MidEnergize extends MidJson
         try {
             $content = $next();
 
+            $content = Page::renderize($content);
+
             if (is_array($content) || is_json($content))
                 parent::encaps($content);
             else
-                Response::content(Page::renderize($content));
+                Response::content($content);
 
             Response::send();
         } catch (Error | Exception $e) {
 
-            if ($e->getCode() == STS_REDIRECT)
+            if ($e->getCode() == STS_REDIRECT) {
+
+                if (IS_ENERGIZE) {
+                    $url = $e->getMessage();
+                    $url = !empty($url) ? url($url) : url('.');
+                    $response = [
+                        'info' => [
+                            'elegance' => true,
+                            'status' => STS_REDIRECT,
+                            'error' => false,
+                            'location' => $url,
+                        ],
+                        'data' => null
+                    ];
+
+                    Response::header('Energize-Location', $url);
+                    Response::content($response);
+                    Response::status(STS_OK);
+                    Response::type('json');
+                    Response::send();
+                }
+
                 throw $e;
+            }
 
             if (IS_ENERGIZE) {
                 parent::encapsCatch($e);
